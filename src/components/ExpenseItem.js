b53a4df6d2f59a1ce750e22e00a7c8c1a8fbc3ec
ninjaJason86 <jsntrout@gmail.com>
2024-01-28 17:45:33 +0200

    Task 7: Decrease and Increase Buttons: Add a decrease button, like the increase button you added in the Practice Assignment - Hands-on Lab: Budget Allocation Application, to all the departments in the allocation, beside increase by 10 as shown in the screenshot. Stylize the button using these images. Styling of buttong should be done according to the reference provided. Click the plus button next to Marketing department and show that the the value increases by 10. Take a screenshot and save it as mktgplus10.png and Click the minus button next to IT and show that the the value decreases by 10. Take a screenshot and save it as itminus10.png

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,61 @@
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import { TiDelete } from "react-icons/ti";
+import { RiAddCircleFill } from "react-icons/ri";
+import { FaMinusCircle, FaPlusCircle } from "react-icons/fa";
 
+
+
+export default function ExpenseItem(props) {
+    const { currency, dispatch } = useContext(AppContext);
+
+
+    function handleIncreaseAllocationBy10(item) {
+        dispatch({
+            type: "ADD_EXPENSE",
+            payload: { ...item, cost: 10, }
+        });
+    }
+
+    function handleDecreaseAllocationBy10(item) {
+        dispatch({
+            type: "REDUCE_EXPENSE",
+            payload: { ...item, cost: 10, }
+        });
+    }
+
+
+    function handleDeleteExpense(item) {
+        dispatch({
+            type: "DELETE_EXPENSE",
+            payload: item.id,
+        });
+    }
+
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>{currency}{props.cost}</td>
+            <td>
+                <FaPlusCircle
+                    size={"40px"}
+                    color="green"
+                    onClick={() => handleIncreaseAllocationBy10(props)}
+                />
+            </td>
+            <td>
+                <FaMinusCircle
+                    size={"40px"}
+                    color="darkred"
+                    onClick={() => handleDecreaseAllocationBy10(props)}
+                />
+            </td>
+            <td>
+                <TiDelete
+                    size="1.5em"
+                    onClick={() => handleDeleteExpense(props)}
+                />
+            </td>
+        </tr>
+    );
+}
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,26 +1,11 @@
 import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
-import { TiDelete } from "react-icons/ti";
+import ExpenseItem from "./ExpenseItem";
 
 export default function ExpenseList() {
-    const { expenses, currency, dispatch } = useContext(AppContext);
+    const { expenses, } = useContext(AppContext);
 
 
-
-    function handleIncreaseAllocationBy10(item) {
-        dispatch({
-            type: "ADD_EXPENSE",
-            payload: { ...item, cost: 10, }
-        });
-    }
-
-
-    function handleDeleteExpense(item) {
-        dispatch({
-            type: "DELETE_EXPENSE",
-            payload: item.id,
-        });
-    }
 
     return (
         <table className="table">
@@ -29,18 +14,12 @@ export default function ExpenseList() {
                     <th scope="col">Department</th>
                     <th scope="col">Allocated Budget</th>
                     <th scope="col">Increase by 10</th>
+                    <th scope="col">Decrease by 10</th>
                     <th scope="col">Delete</th>
                 </tr>
             </thead>
             <tbody>
-                {expenses.map((item) => (
-                    <tr>
-                        <td>{item.name}</td>
-                        <td>{currency}{item.cost}</td>
-                        <td><button onClick={() => handleIncreaseAllocationBy10(item)}>+</button></td>
-                        <td><TiDelete size="1.5em" onClick={() => handleDeleteExpense(item)}></TiDelete></td>
-                    </tr>
-                ))}
+                {expenses.map(ExpenseItem)}
             </tbody>
         </table>
     )
